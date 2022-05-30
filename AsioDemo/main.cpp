7ce6c63e8a451ade5d7d761682ac33bdf54e365f
zz <35785077@qq.com>
2022-05-30 16:56:36 +0800

    server

--- AsioDemo/main.cpp
+++ AsioDemo/main.cpp
@@ -1,112 +1,58 @@
-﻿//
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include <cstdlib>
+﻿#include <cstdlib>
 #include <iostream>
-#include <memory>
+#include <thread>
 #include <utility>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
 using asio::ip::tcp;
 
-class session
-    : public std::enable_shared_from_this<session>
+const int max_length = 1024;
+
+void session(tcp::socket sock)
 {
-public:
-    session(tcp::socket socket)
-        : socket_(std::move(socket))
+    try
     {
-    }
+        for (;;)
+        {
+            char data[max_length];
 
-    void start()
+            std::error_code error;
+            size_t length = sock.read_some(asio::buffer(data), error);
+            if (error == asio::stream_errc::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw std::system_error(error); // Some other error.
+
+            asio::write(sock, asio::buffer(data, length));
+        }
+    }
+    catch (std::exception& e)
     {
-        do_read();
+        std::cerr << "Exception in thread: " << e.what() << "\n";
     }
+}
 
-private:
-    void do_read()
-    {
-        auto self(shared_from_this());
-        socket_.async_read_some(asio::buffer(data_, max_length),
-            [this, self](std::error_code ec, std::size_t length)
-            {
-                if (!ec)
-                {
-                    do_write(length);
-                }
-            });
-    }
-
-    void do_write(std::size_t length)
-    {
-        auto self(shared_from_this());
-        asio::async_write(socket_, asio::buffer(data_, length),
-            [this, self](std::error_code ec, std::size_t /*length*/)
-            {
-                if (!ec)
-                {
-                    do_read();
-                }
-            });
-    }
-
-    tcp::socket socket_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-};
-
-class server
+void server(asio::io_context& io_context, unsigned short port)
 {
-public:
-    server(asio::io_context& io_context, short port)
-        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)),
-        socket_(io_context)
+    tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
+    for (;;)
     {
-        do_accept();
+        tcp::socket sock(io_context);
+        a.accept(sock);
+        std::thread(session, std::move(sock)).detach();
     }
-
-private:
-    void do_accept()
-    {
-        acceptor_.async_accept(socket_,
-            [this](std::error_code ec)
-            {
-                if (!ec)
-                {
-                    std::make_shared<session>(std::move(socket_))->start();
-                }
-
-                do_accept();
-            });
-    }
-
-    tcp::acceptor acceptor_;
-    tcp::socket socket_;
-};
+}
 
 int main(int argc, char* argv[])
 {
     try
     {
-        //if (argc != 2)
-        //{
-        //    std::cerr << "Usage: async_tcp_echo_server <port>\n";
-        //    return 1;
-        //}
+        char const* port = "8888";
 
         asio::io_context io_context;
 
-        server s(io_context, std::atoi("8888"));
-
-        io_context.run();
+        server(io_context, std::atoi(port));
     }
     catch (std::exception& e)
     {
